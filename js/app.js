9a319773e8b488813920e4dc42b8c163003196c6
Cphil31 <cecilephilippe31@gmail.com>
2020-08-21 15:57:49 +0200

    add alert

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 window.onload = function () {
 
+let body = document.body;
+
+let titre = document.createElement("h1");
+titre.innerHTML="keydown";
+body.appendChild(titre);
+
+
+    document.addEventListener('keydown', (event) => {
+        const nomTouche = event.key;
+      
+        if (nomTouche === 'Control') {
+          // Pas d'alerte si seule la touche Control est pressée.
+          return;
+        }
+      
+        if (event.ctrlKey) {
+          // Même si event.key n'est pas 'Control' (par ex., 'a' is pressed),
+          // event.ctrlKey peut être true si la touche Ctrl est pressée dans le même temps.
+          alert(`Combinaison de ctrlKey + ${nomTouche}`);
+        } else {
+          alert(`Touche pressée ${nomTouche}`);
+        }
+      }, false);
+
 }
